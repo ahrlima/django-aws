d90cdf4eaf0e13cf8de86bfedcc85363d8be98f3
Anderson Lima <anderson.lima873@matera.com>
2025-10-15 20:44:44 -0300

    new test

--- app/testapp/views.py
+++ app/testapp/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def hello_world(request):
-    return HttpResponse('Put the faith in the light', status=200)
+    return HttpResponse('Put yours faith in the light', status=200)
 
 
 def health_check(request):
