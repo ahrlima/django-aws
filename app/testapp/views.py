545f39f686d811c4ecd9f034bf840e152471d587
Anderson Lima <anderson.lima873@matera.com>
2025-10-15 21:11:06 -0300

    final test

--- app/testapp/views.py
+++ app/testapp/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def hello_world(request):
-    return HttpResponse('in the light', status=200)
+    return HttpResponse('Hello World', status=200)
 
 
 def health_check(request):
